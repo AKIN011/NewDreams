80b5baf8faa3acb0d526183e9d0685030e207bb5
Felipe Rodriguez <dfelipebr737@gmail.com>
2024-02-13 23:13:00 -0500

    Se sube el formulario de servicios

---
+++ src/java/Controlador/serviciosServlet.java
@@ -0,0 +1,38 @@
+import DAO.servicioDAO;
+import Modelo.conexion;
+import Modelo.servicio;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class serviciosServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Crear instancia de conexion
+        conexion con = new conexion();
+        
+        // Obtener conexión a la base de datos
+        Connection connection = con.conectar();
+        
+        // Crear instancia de servicioDAO
+        servicioDAO servicioDAO = new servicioDAO(connection);
+
+        try {
+            // Consultar todos los servicios
+            List<servicio> servicios = servicioDAO.consultarTodosServicios();
+
+            // Agregar la lista de servicios como atributo de solicitud
+            request.setAttribute("servicios", servicios);
+
+            // Enviar la solicitud al JSP
+            request.getRequestDispatcher("form.jsp").forward(request, response);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Manejo de errores
+        }
+    }
+}
---
+++ src/java/DAO/servicioDAO.java
@@ -0,0 +1,37 @@
+package DAO;
+
+
+import Modelo.servicio;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class servicioDAO {
+    private Connection connection;
+
+    public servicioDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    //Metodo para consultar los registros de la base de datos en la tabla servicios
+    public List<servicio> consultarTodosServicios() throws SQLException {
+        List<servicio> servicios = new ArrayList<>();
+        String query = "SELECT * FROM servicio";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String idServicio = resultSet.getString("idServicio");
+                int valorServicio = resultSet.getInt("Valor_Servicio");
+                String tipoServicio = resultSet.getString("Tipo_Servicio");
+                String descripcionServicio = resultSet.getString("Descripcion_servicio");
+                  System.out.println(tipoServicio);
+                servicios.add(new servicio(idServicio, valorServicio, tipoServicio, descripcionServicio));
+            }
+        }
+        return servicios;
+    }
+}
+
